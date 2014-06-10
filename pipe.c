6708bc1deede4e635d781ef0e7fe65165c1e8171
TH3Mjuss <mehdi.jusseaume@epitech.eu>
2014-06-10 14:59:48 +0200

    Merging Badrou OP !

--- pipe.c
+++ pipe.c
@@ -5,7 +5,7 @@
 ** Login   <jussea_m@epitech.net>
 **
 ** Started on  Mon Jun  9 17:44:30 2014 jussea_m@epitech.eu
-** Last update Tue Jun 10 14:13:29 2014 jussea_m@epitech.eu
+** Last update Tue Jun 10 14:49:57 2014 jussea_m@epitech.eu
 */
 
 #include "ufo.h"
@@ -29,15 +29,15 @@ int     mission3(char **tab)
   int	fd[2];
 
   if (pipe(fd) == -1)
-    exit(fprintf(stderr, "genealfs: pipe error\n"));
+    return (write(2, "Genealfs: Error\n", 16));
   if ((pid = fork()) < 0)
-     exit(-1);
+     return (-1);
   if (pid == 0)
     {
       close(fd[0]);
       dup2(fd[1], 1);
       genealfs(tab[1], 1);
-      exit(0);
+      return (0);
     }
   else
     {
@@ -45,7 +45,7 @@ int     mission3(char **tab)
       dup2(fd[0], 0);
       execvp(tab[3], tab + 3);
       wait(NULL);
-      exit(0);
+      return (0);
     }
   return (0);
 }
