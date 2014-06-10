cb10e0cf629b20628c6488b0bdd0368aeb9cae02
Zackary <zackary.beaugelin@epitech.eu>
2014-06-10 11:13:00 +0200

    main propre

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 ** Login   <gysc0@epitech.net>
 **
 ** Started on  Mon Jun  9 10:16:21 2014 Zackary Beaugelin
-** Last update Tue Jun 10 09:26:28 2014 Gysc0
+** Last update Tue Jun 10 11:11:39 2014 Gysc0
 */
 
 #include "ufo.h"
@@ -17,10 +17,7 @@ void		display(struct dirent *d, struct stat info, int fd, char *path)
   char		*s;
 
   bzero(buff, 4096);
-  if (path[0] != '.' && path[1] != '/')
-    s = my_strcat(my_strcat("./", path), my_strcat("/", d->d_name));
-  else
-    s = my_strcat(path, my_strcat("/", d->d_name));
+  s = my_strcat(path, d->d_name);
   realpath(s, buf);
   read(open(s, O_RDONLY, 0444), buff, 4096);
   stat(s, &info);
@@ -28,12 +25,15 @@ void		display(struct dirent *d, struct stat info, int fd, char *path)
   s++;
   write(fd, "name: ", 6);
   write(fd, s, strlen(s));
-  write(fd, " birth: ", 8);
+  if (strlen(s) < 10)
+    write(fd, "\t\tbirth: ", 9);
+  else
+    write(fd, "\tbirth: ", 8);
   if (buff[strlen(buff) - 1] == '\n')
     write(fd, buff, strlen(buff) - 1);
   else
     write(fd, buff, strlen(buff));
-  write(fd, " at: ", 5);
+  write(fd, "\tat: ", 5);
   my_putnbr(fd, info.st_uid);
   write(fd, "\n", 1);
 }
@@ -41,18 +41,30 @@ void		display(struct dirent *d, struct stat info, int fd, char *path)
 void		genealfs(char *path, int fd)
 {
   DIR		*dirp;
+  char		buf[4096];
+  char		*s;
   struct stat	info;
   struct dirent *d;
 
   if (!(dirp = opendir(path)))
-    exit(fprintf(stderr, "Error opening directory, maybe not a directory\n"));
-  if (!(d = readdir(dirp)))
-    exit(fprintf(stderr, "Error reading directory\n"));
+    exit(fprintf(stderr, "Error opening directory: %s, maybe not a directory\n", path));
   while ((d = readdir(dirp)))
     {
-      if (d == NULL)
-	exit(0);
-      if (d->d_name[0] != '.')
+      if (d->d_type == DT_DIR)
+	{
+	  if (strcmp(d->d_name, ".") && strcmp(d->d_name, ".."))
+	    {
+	      write(fd, "family: ", 8);
+	      write(fd, d->d_name, strlen(d->d_name));
+	      write(fd, "\n", 1);
+	      realpath(my_strcat(path, my_strcat("/", d->d_name)), buf);
+	      s = my_strcat(buf, "/");
+	      genealfs(s, fd);
+	      write(fd, "______________________________\n", 31);
+	    }
+	}
+      else if (d->d_name[0] != '.' && strcmp(d->d_name, "father")
+	       && strcmp(d->d_name, "mother"))
 	display(d, info, fd, path);
     }
 }
